141cafefd80874187ac14c9c0a0e852e903933e2
LauraSinitsyna <laura.vescu@gmail.com>
2021-12-20 10:01:25 +0200

    correction

--- 4.py
+++ 4.py
@@ -1,19 +1,3 @@
-'''num = input('Enter number : ')
-num = int(num)
-res1 = num // 10
-var2 = num % 10
-res2 = var2 // 10
-res3 = num % 10
-mx = res1
-
-while True:
-    if res2 > mx:
-        mx = res2
-    if res3 > mx:
-        mx = res3
-    print(mx)
-    break'''
-
 num = int(input('Enter number : '))
 maxInt = 0
 
