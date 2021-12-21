be07af2d1d1ce07ed7f98a04d7b32e21b6c11b82
LauraSinitsyna <laura.vescu@gmail.com>
2021-12-21 10:53:41 +0200

    next

---
+++ 1.py
@@ -0,0 +1,3 @@
+data = [5, 'Crash', 3 + 1j, True, 12.36, 'Smile me', False, ['the', 'sun'], {'changer': 'what', 'time': 'move on'}]
+for elem in data:
+    print(type(elem))
---
+++ 2.py
@@ -0,0 +1,7 @@
+data = input('Input data list using space characters: ')
+for
+new_str = data.split(' ')
+print(new_str)
+'''for elem in data:
+    print(elem)
+'''
