94c581fc6ae064df48bf1a5d61d3aa650119c549
LauraSinitsyna <laura.vescu@gmail.com>
2021-12-26 22:48:39 +0200

    hmw3

--- 2.py
+++
@@ -1,4 +0,0 @@
-data = input('Input data list using space characters: ').split(' ')
-for elem in range(1, len(data), 2):
-    data[elem-1], data[elem] = data[elem], data[elem-1]
-print(data)
--- 4.py
+++
@@ -1,4 +0,0 @@
-str_ = input('Text: ').split()
-for elem in str_:
-    print(elem[:10])
-    
