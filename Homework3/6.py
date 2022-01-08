2b083a7b0a9370d22c1bba8eab80c842b9fa7142
LauraSinitsyna <laura.vescu@gmail.com>
2022-01-08 20:16:39 +0200

    hmw3-1/8/22

--- Homework3/6.py
+++ Homework3/6.py
@@ -1,5 +1,10 @@
 def int_func(message):
-    latin = 'qwertyuiopasdfghjklzxcvbnm'
-    return message.ti if not set(message).difference(latin) else False
+    latin_char = 'qwertyuioplkjhgfdsazxcvbnm'
+    return message.title() if not set(message).difference(latin_char) else False
 
-print('text')
+
+text = input('Your text here: ').split()
+for word in text:
+    result = int_func(word)
+    if result:
+        print(int_func(word), ' ')
